8c4cd0542dbdeec3089259973260c7434763ce0a
Kaushik Mandal <koushik369mondal@gmail.com>
2026-07-22 17:57:20 +0530

    remove unused files

--- server/scratch/test_env.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-console.log("--- Env Var Test ---");
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS ? `Defined (length: ${process.env.EMAIL_PASS.length})` : "Undefined/Empty");
